46e760b05834d64c982cec22b74b1f22abd45b5e
nathancordrey <nathan.cordrey@gmail.com>
2024-01-19 20:48:28 -0500

    added some content and updated title

--- rachelcooks/src/data/categories.ts
+++ rachelcooks/src/data/categories.ts
@@ -1,8 +1,9 @@
 // List of categories for blog posts
 export const CATEGORIES = [
-	'Category 1',
-	'Category 2',
-	'Category 3',
-	'Category 4',
-	'Category 5'
+	'Main Dishes',
+	'Appetizers',
+	'Breads',
+	'Breakfast',
+	'Desserts',
+	'Jams and Preserves'
 ] as const
